2ba5f6ee5c4986d7c91196e5080266dac483f9c5
Francine N <hello@francine.codes>
2014-12-06 23:04:04 -0500

    created highlights.php

---
+++ highlights.php
@@ -0,0 +1,29 @@
+<div class="row">
+	<div class="col-md-4">
+		<h3>Most Recent</h3>
+		<div class="video">
+			
+		</div>
+		<h4 class="incident">Incident</h4>
+		<h4 class="location">Location</h4>
+		<h4 class="date">Date</h4>
+	</div>
+	<div class="col-md-4">
+		<h3>Highest Rated</h3>
+		<div class="video">
+			
+		</div>
+		<h4 class="incident">Incident</h4>
+		<h4 class="location">Location</h4>
+		<h4 class="date">Date</h4>
+	</div>
+	<div class="col-md-4">
+		<h3>Lowest Rated</h3>
+		<div class="video">
+			
+		</div>
+		<h4 class="incident">Incident</h4>
+		<h4 class="location">Location</h4>
+		<h4 class="date">Date</h4>
+	</div>
+</div>
